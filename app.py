f90d19f033e77899cd14e9f0d91b7533f0994e1b
kariseah <101925175+kariseah@users.noreply.github.com>
2022-03-19 17:41:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,36 +10,48 @@ from flask import Flask, request, render_template
 # In[2]:
 
 
+# Anything with __ & __ 
 app = Flask(__name__)
 
 
 # In[3]:
 
 
+# once you press submit, this if will happen (before will go to else)
 import joblib
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET","POST"])
 def index():
     if request.method == "POST":
         rates = request.form.get("rates")
         print(rates)
         model = joblib.load("DBS_Reg")
         pred = model.predict([[float(rates)]])
+        
         s = "The predicted DBS share price is " + str(pred)
-        return(render_template("index.html", results=s))
+        return(render_template("index.html",results=s))
     else:
-        return(render_template("index.html", results="2"))
+        return(render_template("index.html",results="Input the value into the above field"))
+
+
+# In[4]:
+
+
+# cloud environment need this - to make sure that its yours
+
+#if __name__ == "__main__":
+#    app.run()
 
 
 # In[ ]:
 
 
-if __name__=="__main__":
+#beyond repair
+
+if __name__ == "__main__":
     app.run()
 
 
 # In[ ]:
-
-
 
 
